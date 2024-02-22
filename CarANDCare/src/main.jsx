817634512139698fc04860389d810e7343d3e058
AmanAta <95543969+AmanAta@users.noreply.github.com>
2024-02-22 16:31:26 +0530

    fixes routes

--- CarANDCare/src/main.jsx
+++ CarANDCare/src/main.jsx
@@ -2,36 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Home from "./components/Home/Home"
 import Services from "./components/Services/Services"
 import Galary from "./components/Galary/Galary"
 import Contact from "./components/Contact/Contact"
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    children:[
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "/services",
-        element: <Services />
-      },
-      {
-        path: "/galary",
-        element: <Galary />
-      },
-      {
-        path: "/contact",
-        element: <Contact />
-      },
-    ]
-  }
-])
+
+// m1
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App/>,
+//     children:[
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "/services",
+//         element: <Services />
+//       },
+//       {
+//         path: "/galary",
+//         element: <Galary />
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />
+//       },
+//     ]
+//   }
+// ])/
+
+
+// m2
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />} />
+      <Route path='services' element={<Services />} />
+      <Route path='galary' element={<Galary />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
